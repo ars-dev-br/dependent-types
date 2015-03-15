bccee693cc9408210b906434bb2b7282b2e0c361
Andre Ramaciotti da Silva <andre@ramaciotti.com>
2015-03-14 21:27:34 -0300

    Starting type validation.

--- src/lib/DependentTypes/Semantic.hs
+++ src/lib/DependentTypes/Semantic.hs
@@ -4,11 +4,14 @@ Description: Semantic analysis and program interpreter.
 -}
 
 module DependentTypes.Semantic
-       ( nullEnv
+       ( Env
        , evalProgram
        , fromList
+       , nullEnv
        ) where
 
+import Control.Exception
+import Control.Monad
 import Data.IORef
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -37,10 +40,32 @@ evalToplevels env (t:ts) = do
 
 -- | Evaluates a toplevel construct.
 evalToplevel :: Env -> Toplevel -> IO ()
-evalToplevel env t@(Type name _ _)       = modifyIORef env (Map.insert name t)
+evalToplevel env t@(Type name sig cons)       = do
+  checkTypeSignature env name sig
+  modifyIORef env (Map.insert name t)
 evalToplevel env   (Func []     lambdas) = return ()
 evalToplevel env f@(Func (s:ss) lambdas) = do
   modifyIORef env (Map.insert (fst s) $ Func [s] lambdas)
   evalToplevel env $ Func ss lambdas
 evalToplevel env print@(Print exp) = do
   return ()
+
+-- | Checks if a signature for a type is valid
+checkTypeSignature :: Env -> String -> Signature -> IO ()
+checkTypeSignature env name (Signature []) = error $ name ++ ": " ++ "Type must have signature"
+checkTypeSignature env name (Signature [s]) = do
+  unless (s == "Type") $ error (name ++ ": " ++ "Type signature must end with 'Type'.")
+checkTypeSignature env name (Signature (s:ss)) = do
+  e <- readIORef env
+  unless (s == "Type" || isType e s) $
+    error (name ++ ": " ++ "Type signature must have already defined types.")
+  checkTypeSignature env name (Signature ss)
+
+-- | Checks if a type name refers to an existing type.
+isType :: Map String Toplevel -> TypeDef -> Bool
+isType m (TypeId name) = case Map.lookup name m of
+                          Just (Type _ _ _) -> True
+                          _                 -> False
+isType m (DepType name _) = case Map.lookup name m of
+                             Just (Type _ _ _) -> True
+                             _                 -> False
--- tests/Semantic.hs
+++ tests/Semantic.hs
@@ -4,6 +4,7 @@ import Control.Exception
 import Data.IORef
 import Data.Map (Map)
 import DependentTypes.Data
+import DependentTypes.Parser
 import DependentTypes.Semantic
 import Test.Tasty
 import Test.Tasty.HUnit hiding (assertEqual)
@@ -15,15 +16,24 @@ assertEqual envRef1 envRef2 = do
   env2 <- readIORef envRef2
   HUnit.assertEqual "" env1 env2
 
-testSimpleEval :: String -> String -> Toplevel -> TestTree
-testSimpleEval title id value = testCase title $
-                                do
-                                  env1 <- nullEnv
-                                  env2 <- fromList [(id, value)]
+assertException :: Env -> Program -> IO ()
+assertException env p = do
+  except <- try $ evalProgram env p :: IO (Either ErrorCall ())
+  case except of
+   Right p -> assertFailure "Invalid program was accepted."
+   Left  e -> return ()
 
-                                  evalProgram env1 $ Program [value]
-                                  assertEqual env1 env2
+nat :: Toplevel
+nat = Type "Nat" (Signature ["Type"]) [ Constructor "zero" (Args []) (Signature ["Nat"])
+                                        NoConstraint
+                                      , Constructor "suc" (Args []) (Signature ["Nat", "Nat"])
+                                        NoConstraint ]
 
+bool :: Toplevel
+bool = Type "Bool" (Signature ["Type"]) [ Constructor "false" (Args []) (Signature ["Bool"])
+                                          NoConstraint
+                                        , Constructor "true" (Args []) (Signature ["Bool"])
+                                          NoConstraint ]
 
 semanticTests = testGroup "Semantic" [emptyTests, typeTests, funcTests, printTests]
 
@@ -38,21 +48,108 @@ emptyTests = testGroup "Empty Program"
   ]
 
 typeTests = testGroup "Type Definitions"
-  [ testSimpleEval "Empty type" "Void" $ Type "Void" (Signature ["Type"]) []
+  [ testCase "Empty type" $
+    do
+      env1 <- nullEnv
+      env2 <- fromList [("Void", Type "Void" (Signature ["Type"]) [])]
+
+      case parse "type Void : Type." of
+       Right p -> evalProgram env1 p
+       Left  e -> assertFailure $ show e
+
+      assertEqual env1 env2
+
+  , testCase "Unit type" $
+    do
+      env1 <- nullEnv
+      env2 <- fromList [("Unit", Type "Unit" (Signature ["Type"])
+                                      [Constructor "unit" (Args []) (Signature ["Unit"]) NoConstraint])]
+
+      case parse "type Unit : Type where unit : Unit." of
+       Right p -> evalProgram env1 p
+       Left  e -> assertFailure $ show e
+
+      assertEqual env1 env2
+
+  , testCase "Type definition not having a type signature" $
+    do
+      env <- nullEnv
+      case parse "type Void : Error." of
+       Right p -> assertException env p
+       Left  e -> assertFailure $ show e
+
+  , testCase "Type definition depending on undefined type" $
+    do
+      env <- nullEnv
+      case parse "type Error : Nat -> Type." of
+       Right p -> assertException env p
+       Left  e -> assertFailure $ show e
+
+  , testCase "Type definition depending on defined type" $
+    do
+      env <- nullEnv
+      case parse "type Void : Type. type Valid : Void -> Type." of
+       Right p -> evalProgram env p
+       Left  e -> assertFailure $ show e
   ]
 
 funcTests = testGroup "Function Definitions"
-  [ testSimpleEval "Simple function" "one" $ Func [("one", Signature ["Nat"])]
-                                                  [Lambda "one" (Args []) (ExpList [ ExpId "suc"
-                                                                                   , ExpId "zero"])]
+  [ testCase "Simple function" $
+    do
+      env1 <- nullEnv
+      env2 <- fromList [ ("Nat", nat)
+                       , ("one", Func [("one", Signature ["Nat"])]
+                                 [Lambda "one" (Args []) (ExpList [ ExpId "suc", ExpId "zero" ])]) ]
+
+      case parse "type Nat : Type where\n\
+                 \  zero : Nat;\n\
+                 \  suc  : Nat -> Nat.\n\
+                 \\n\
+                 \func one : Nat where\n\
+                 \  one = suc zero." of
+       Right p -> evalProgram env1 p
+       Left  e -> assertFailure $ show e
+
+      assertEqual env1 env2
 
   , testCase "Mutually recursive function" $
     do
+      oddEven <- return $ [ Lambda "odd" (Args [ExpId "zero"]) (ExpList [ExpId "false"])
+                          , Lambda "even" (Args [ExpId "zero"]) (ExpList [ExpId "true"])
+                          , Lambda "odd" (Args [ExpList [ExpId "suc", ExpId "x"]])
+                                   (ExpList [ExpId "even", ExpId "x"])
+                          , Lambda "even" (Args [ExpList [ExpId "suc", ExpId "x"]])
+                                   (ExpList [ExpId "odd", ExpId "x"]) ]
+
       env1 <- nullEnv
-      env2 <- fromList []
-      evalProgram env1 $ Program []
+      env2 <- fromList [ ("Nat", nat), ("Bool", bool)
+                       , ("odd", Func [("odd", Signature ["Nat", "Bool"])] oddEven)
+                       , ("even", Func [("even", Signature ["Nat", "Bool"])] oddEven) ]
+
+      case parse "type Nat : Type where\n\
+                 \  zero : Nat;\n\
+                 \  suc  : Nat -> Nat.\n\
+                 \\n\
+                 \type Bool : Type where\n\
+                 \  false : Bool;\n\
+                 \  true  : Bool.\n\
+                 \\n\
+                 \func odd : Nat -> Bool; even : Nat -> Bool where\n\
+                 \  odd  zero    = false;\n\
+                 \  even zero    = true;\n\
+                 \  odd (suc x)  = even x;\n\
+                 \  even (suc x) = odd x." of
+       Right p -> evalProgram env1 p
+       Left  e -> assertFailure $ show e
 
       assertEqual env1 env2
+
+  ,  testCase "Function definition for undefined type" $
+     do
+       env <- nullEnv
+       case parse "func not : Bool -> Bool where not true = false; not false = true." of
+        Right p -> assertException env p
+        Left  e -> assertFailure $ show e
   ]
 
 printTests = testGroup "Print Statements"
